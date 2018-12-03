62046cd6ec2a64d5f677ab1f64280539250f2676
Christian Sandén <ccsanden@gmail.com>
2018-12-03 21:01:16 +0100

    1.2

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <chrono>
+
 int main(const int argc, char const* argv[])
 {
     int32_t sum = 0;
+    std::vector<int32_t> inputs;
+    std::vector<int32_t> seen_frequencies;
+
     for (std::string line; std::getline(std::cin, line);) {
-        sum += std::stoi(line);
+        inputs.push_back(std::stoi(line));
     }
-    std::cout << "Result: " << sum << "\n";
-    return 0;
+
+    seen_frequencies.reserve(inputs.size());
+
+    for (const auto i : inputs) {
+        sum += i;
+        seen_frequencies.push_back(sum);
+    }
+
+    while (true) {
+        for (const auto i : inputs) {
+            sum += i;
+            for (const auto f : seen_frequencies) {
+                if (f == sum) {
+                    std::cout << "Result: " << f << "\n";
+                    return 0;
+                }
+            }
+        }
+    }
+
+    return 1;
 }
